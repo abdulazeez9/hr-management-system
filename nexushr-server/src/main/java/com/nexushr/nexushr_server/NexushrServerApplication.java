758bdad9924ef482c117e48bd89d4914b20e3411
muritador5050 <muritador5050@gmail.com>
2025-11-26 10:15:06 +0100

    intialiized spring boot

---
+++ nexushr-server/src/main/java/com/nexushr/nexushr_server/NexushrServerApplication.java
@@ -0,0 +1,13 @@
+package com.nexushr.nexushr_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexushrServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexushrServerApplication.class, args);
+	}
+
+}
---
+++ nexushr-server/src/test/java/com/nexushr/nexushr_server/NexushrServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexushr.nexushr_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexushrServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
