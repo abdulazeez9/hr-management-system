15a5996040ae89660bcc5a8979e3713d153eb365
muritador5050 <muritador5050@gmail.com>
2025-12-18 22:38:06 +0100

    fix:i separated some config between application.properties files

--- nexushr-server/src/main/java/com/nexushr/nexushr_server/modules/tenant/TenantRepository.java
+++ nexushr-server/src/main/java/com/nexushr/nexushr_server/modules/tenant/TenantRepository.java
@@ -2,7 +2,6 @@ package com.nexushr.nexushr_server.modules.tenant;
 
 import java.util.Optional;
 import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
